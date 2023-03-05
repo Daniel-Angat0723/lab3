e7fd948348bf746b2c24c81c8dc58d7d813280ea
Daniel-Angat0723 <84165807+Daniel-Angat0723@users.noreply.github.com>
2023-03-05 14:06:14 +0800

    Futher CSS fix

--- ci4/app/Views/pages/guest.php
+++ ci4/app/Views/pages/guest.php
@@ -2,14 +2,15 @@
                 <div class="row pb-4" data-aos="fade-up">
                     <div class="col-lg-8">
                         <h6 class="text-brand">MY GUESTS</h6>
-                        <h1>The army!</h1>
+                        <h1>The members of the kawasaki army! Please be updated of our page to be aware of future group rides.</h1>
                     </div>
                 </div>
                 <div class="row gy-4" data-aos="fade-up">
                     <?php if(!empty($guest) && is_array($guest)):?>
                         <?php foreach($guest as $guest_item): ?>
                             <div class="col-md-3" data-aos="fade-up">
-                                <h3 class="text-brand"><?= esc($guest_item['fname'])?> <?= esc($guest_item['lname'])?><h5> - <?= esc($guest_item['tagline'])?></h5></h3>
+                                <h3 class="text-brand"><?= esc($guest_item['fname'])?> <?= esc($guest_item['lname'])?></h3>
+                                <p><h5>Tagline: <?= esc($guest_item['tagline'])?></h5></p>
                             </div>
                         <?php endforeach ?>
                     <?php else: ?>
